e0aee5a2379742bb6709bc15810b14b98df2e6e8
Julee Anjana <julieanjana2002@gmail.com>
2025-02-08 23:31:37 +0530

    update

--- aac.c
+++ aac.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
-       printf("Welcome to print Practical for fundamental Programming!\n");
+	printf("Welcome to print Practical for fundamental Programming!\n");
+	printf("Welcome to print Practical!\n");
        
        return 0;
 }
